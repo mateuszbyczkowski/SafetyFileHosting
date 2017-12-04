bc448db6879c8791d4f6c83d3ccf1157d86ebd3f
mateusz <byczkowski38@gmail.com>
2017-12-04 09:26:27 +0100

    Created controllers, added and modified views

---
+++ SafetyFileHosting/Controllers/CertificatesManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SafetyFileHosting.Controllers
+{
+    public class CertificatesManagerController : Controller
+    {
+        // GET: CertificatesManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SafetyFileHosting/Controllers/FilesBrowserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SafetyFileHosting.Controllers
+{
+    public class FilesBrowserController : Controller
+    {
+        // GET: FilesBrowser
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
